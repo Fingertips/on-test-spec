61835668d360640fbab53866ddae76215a6bca90
Eloy Duran <eloy.de.enige@gmail.com>
2009-09-11 14:28:05 +0200

    Removed "add all assertions to Test::Unit::TestCase"-hack and started on using the TestCase class that belongs to the class given to describe. The class given to describe will also be used for the description.
    Still need to refactor it.

--- lib/test/spec/rails.rb
+++ lib/test/spec/rails.rb
@@ -1,27 +1,15 @@
 require 'test/spec'
 
 require 'active_support/test_case'
+
+require 'active_record'
+require 'active_record/test_case'
+
 require 'action_controller'
 require 'action_controller/test_case'
 
-module Test
-  module Spec
-    module Rails
-      module Assertions
-        include ActiveSupport::Testing::Assertions
-        include ActionController::TestCase::Assertions
-      end
-    end
-    
-    class Should
-      include Rails::Assertions
-    end
-    
-    class ShouldNot
-      include Rails::Assertions
-    end
-  end
-end
+require 'action_view'
+require 'action_view/test_case'
 
 %w(test_spec_ext spec_responder expectations).each { |lib| require "test/spec/rails/#{lib}" }
 Dir[File.dirname(__FILE__) + '/rails/**/*_helpers.rb'].each { |lib| require lib }
@@ -30,7 +18,31 @@ module Kernel
   alias :context_before_on_test_spec :context
   alias :xcontext_before_on_test_spec :xcontext
   
-  def context(name, superclass=ActiveSupport::TestCase, klass=Test::Spec::TestCase, &block)
+  # def context(name, superclass=ActiveSupport::TestCase, klass=Test::Spec::TestCase, &block)
+  def context(*args, &block)
+    name_parts = []
+    tests = nil
+    
+    until args.empty? || (args.first.is_a?(Class) && args.first.ancestors.include?(Test::Unit::TestCase))
+      arg = args.shift
+      name_parts << arg.to_s
+      tests = arg if arg.is_a?(Module)
+    end
+    
+    name = name_parts.join(' ')
+    
+    superclass = if tests.nil?
+      ActiveSupport::TestCase
+    elsif tests.ancestors.include?(ActiveRecord::Base)
+      ActiveRecord::TestCase
+    elsif tests.ancestors.include?(ActionController::Base)
+      ActionController::TestCase
+    elsif tests.to_s.ends_with?('Helper')
+      ActionView::TestCase
+    end
+    
+    klass=Test::Spec::TestCase
+    
     context_before_on_test_spec(name, superclass, klass, &block)
   end
   
--- test/rails_test.rb
+++ test/rails_test.rb
@@ -1,14 +1,79 @@
-require File.expand_path('../test_helper', __FILE__)
-require 'test/spec/rails'
+require File.expand_path("../test_helper", __FILE__)
+require "test/spec/rails"
 
-describe "A test/spec/rails test case" do
+describe "A test case with a traditional, string only, description" do
   test_case = self
   
-  it "should include the ActiveSupport assertions" do
-    test_case.ancestors.should.include ActiveSupport::Testing::Assertions
+  it "should inherit from ActiveSupport::TestCase" do
+    test_case.superclass.should.be ActiveSupport::TestCase
   end
   
-  it "should include the ActionController assertions" do
-    test_case.ancestors.should.include ActionController::TestCase::Assertions
+  it "should have the description given" do
+    test_case.name.should ==
+      "A test case with a traditional, string only, description"
   end
+end
+
+class ActiveRecordModel < ActiveRecord::Base; end
+class ActionControllerClass < ActionController::Base; end
+module ViewModuleHelper; end
+
+{
+  ActiveRecordModel     => ActiveRecord::TestCase,
+  ActionControllerClass => ActionController::TestCase,
+  ViewModuleHelper      => ActionView::TestCase
+
+}.each do |test_class, expected_test_case|
+
+  describe test_class do
+    test_case = self
+
+    it "should inherit from #{expected_test_case}" do
+      test_case.superclass.should.be expected_test_case
+    end
+
+    it "should use the model class name as the description" do
+      test_case.name.should == test_class.to_s
+    end
+  end
+
+  describe "A test case with prepended description for", test_class do
+    test_case = self
+
+    it "should inherit from #{expected_test_case}" do
+      test_case.superclass.should.be expected_test_case
+    end
+
+    it "should have appended the model class name to the description" do
+      test_case.name.should ==
+        "A test case with prepended description for #{test_class}"
+    end
+  end
+
+  describe test_class, "test case with a model class and an appended description" do
+    test_case = self
+
+    it "should inherit from #{expected_test_case}" do
+      test_case.superclass.should.be expected_test_case
+    end
+
+    it "should have prepended the model class name to the description" do
+      test_case.name.should ==
+        "#{test_class} test case with a model class and an appended description"
+    end
+  end
+
+  describe "An", test_class, "test case with a model class and a prepended _and_ appended description" do
+    test_case = self
+
+    it "should inherit from #{expected_test_case}" do
+      test_case.superclass.should.be expected_test_case
+    end
+
+    it "should have inserted the model class name in the middle of the description" do
+      test_case.name.should ==
+        "An #{test_class} test case with a model class and a prepended _and_ appended description"
+    end
+  end
+
 end
